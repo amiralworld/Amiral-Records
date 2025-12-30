51aba12edac1cc6b3a10ef15dacfa244d23a7902
amiralworld <unitedmusic.mg@gmail.com>
2025-12-30 18:45:30 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+// data.js
+const projectData = [
+    {
+        id: 1,
+        title: "NEVERLOSE", // Proje ismi
+        image: "project1.jpeg",  // Resim dosya adı (klasörde olmalı)
+        link: "https://open.spotify.com/artist/6A8O9h4O7LGwoLbgH1PRkG"               // Tıklayınca gideceği link
+    },
+    {
+        id: 2,
+        title: "cankick",
+        image: "project2.webp",
+        link: "https://open.spotify.com/artist/1l1pQDeLX0hgodwMWlEmAQ"
+    },
+    {
+        id: 3,
+        title: "Dustum",
+        image: "project3.jpeg",
+        link: "https://open.spotify.com/artist/3ctYMEyVCiz7tTiiBVmHWg"
+    }
+    // Yeni proje eklemek için süslü parantez {...} kopyalayıp buraya yapıştır.
+];
---
+++ team.js
@@ -0,0 +1,32 @@
+// team.js - Kurucu ve Yönetim Kadrosu Verileri
+
+const teamData = [
+    {
+        id: 1,
+        name: "SALİH CAN K.",
+        role: "CEO",
+        image: "team1.png", // assets klasörüne bu isimde resim at
+        link: "https://linkedin.com" // Kişinin sosyal medya linki
+    },
+    {
+        id: 2,
+        name: "METEHAN I.",
+        role: "CEO",
+        image: "team2.png",
+        link: "#"
+    },
+    {
+        id: 3,
+        name: "İSMAİL Ç.",
+        role: "CEO",
+        image: "team3.jpeg",
+        link: "#"
+    },
+    {
+        id: 4,
+        name: "RECEP TAYYİP E.",
+        role: "CO-FOUNDER",
+        image: "team4.jpeg",
+        link: "#"
+    }
+];
