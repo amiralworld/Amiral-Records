336449214d607907ddbf1675769a8d7fc7348043
amiralworld <unitedmusic.mg@gmail.com>
2026-01-02 01:05:37 +0300

    Update team.js

--- team.js
+++ team.js
@@ -3,31 +3,32 @@
 const teamData = [
     {
         id: 1,
-        name: "SALİH CAN K.",
-        role: "CEO",
-        image: "team1.png", // assets klasörüne bu isimde resim at
+        name: "",
+        role: "",
+        image: "", // assets klasörüne bu isimde resim at
         link: "https://linkedin.com" // Kişinin sosyal medya linki
     },
     {
         id: 2,
-        name: "İSMAİL Ç.",
-        role: "CEO",
-        image: "team2.png",
+        name: "",
+        role: "",
+        image: "",
         link: "#"
     },
     {
         id: 3,
-        name: "MUZAFFER G.",
-        role: "CEO",
-        image: "team3.jpeg",
+        name: "",
+        role: "",
+        image: "",
         link: "#"
     },
     {
         id: 4,
-        name: "EMİRHAN E.",
-        role: "PRODUCER",
-        image: "team4.jpeg",
+        name: "",
+        role: "",
+        image: "",
         link: "#"
     }
 
 ];
+
